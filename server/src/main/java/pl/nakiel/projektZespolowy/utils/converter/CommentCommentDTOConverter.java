a06d1dc7956e414b874a2b8ed1848e77c3214092
kot <kot@DESKTOP-RNHST0K>
2018-05-04 21:44:51 +0200

    Add comment converter

--- server/src/main/java/pl/nakiel/projektZespolowy/utils/converter/CommentCommentDTOConverter.java
+++ server/src/main/java/pl/nakiel/projektZespolowy/utils/converter/CommentCommentDTOConverter.java
@@ -1,13 +1,22 @@
 package pl.nakiel.projektZespolowy.utils.converter;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import pl.nakiel.projektZespolowy.domain.events.Comment;
+import pl.nakiel.projektZespolowy.domain.security.User;
 import pl.nakiel.projektZespolowy.resources.dto.common.CommentDTO;
 
 @Service
 public class CommentCommentDTOConverter {
+    @Autowired
+    private UserUserDTOConverter userUserDTOConverter;
+
     public CommentDTO toCommentDTO(Comment comment){
-        //TODO uzupelnic
-        return null;
+        CommentDTO commentDTO = new CommentDTO();
+        commentDTO.setComment(comment.getComment());
+        commentDTO.setSubmissionDate(comment.getDate());
+        commentDTO.setId(comment.getId());
+        commentDTO.setUser(userUserDTOConverter.toUserDTO(comment.getCommentsAuthor()));
+        return commentDTO;
     }
 }
